cdd6d2513718aec01c790d2c812261eb0712ed2c
MohammedElmoghazi <P@$etaM)stNeed1>
2023-01-03 15:49:25 +0200

    initial

---
+++ CodeFirst_ConsoleApp1/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeFirst_ConsoleApp1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodeFirst_ConsoleApp1.PlutoContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CodeFirst_ConsoleApp1.PlutoContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ CodeFirst_ConsoleApp1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst_ConsoleApp1
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public CourseLevel Level { get; set; }
+        public string FullPrice { get; set; }
+        public Author Author { get; set; }
+        public IList<Tag> Tags { get; set; }
+
+    }
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Course> Courses { get; set; }
+    }
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Course> Courses { get; set; }
+    }
+    public enum CourseLevel
+    {
+        Beginner = 1,
+        Intermediate = 2,
+        Advanced = 3
+    }
+
+    public class PlutoContext : DbContext
+    {
+        public PlutoContext():base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+    public class Program
+    {
+        /*
+         * steps to use EntityFramework:
+         * install-package EntityFramework -Version:6.4.4
+         * create your classes "models"
+         * create class derived from DbContext Class
+         * Create you DbSets
+         * Add the connection string in your config file
+         * In the your DbContext class in the constractor call base("name=connection string")
+         * In Package manager console enable-migrations , this command run only one time in the project
+         * add-migration migratinname
+         * update-database -verbose
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            Console.ReadLine();
+        }
+    }
+}
